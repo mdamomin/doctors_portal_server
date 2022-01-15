d8a20ac8b9ea111434bec2d11384c597e7b3bca8
MD ABDUL MOMIN <md.mominb@yahoo.com>
2022-01-15 16:33:34 -0600

    local string removed for date

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function run() {
     });
     app.get('/appointments', async (req, res) => {
       const email = req.query.email;
-      const date = new Date(req.query.date).toLocaleDateString();
+      const date = req.query.date;
       const query = { email: email, date: date };
       const cursor = patient_info.find(query);
       const appointments = await cursor.toArray();
